6076eaf07f2c7ec16f5c401d11e4c492f33d2cb4
abdulsalam <abs.pew@gmail.com>
2025-06-29 18:26:48 +0200

    Updated with first test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,22 +23,22 @@ public class Main {
         return num > 0;
     }
 
-    private static String changeToCapitalLetters(String string)
+    public static String changeToCapitalLetters(String string)
     {
         return string.toUpperCase();
     }
 
-    private static int multiplyNumbers(int num1, int num2)
+    public static int multiplyNumbers(int num1, int num2)
     {
        return num1 * num2;
     }
 
-    private static boolean isEven(int num)
+    public static boolean isEven(int num)
     {
         return num % 2 == 0;
     }
 
-    private static int addNumbers(int num1, int num2)
+    public static int addNumbers(int num1, int num2)
     {
         return num1 + num2;
     }
