b100b6a1413c458f3a2871674865ebb2fd6ee32d
abdulsalam <abs.pew@gmail.com>
2025-06-29 17:34:20 +0200

    before TestMethods Main Class & POM

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(addNumbers(4, 5));
+
+       System.out.println(isEven(2));
+
+        System.out.println(multiplyNumbers(4, 5));
+
+        System.out.println(changeToCapitalLetters("neuefische"));
+
+        System.out.println(isPositive(5));
+
+    }
+
+    private static boolean isPositive(int num)
+    {
+        return num > 0;
+    }
+
+    private static String changeToCapitalLetters(String string)
+    {
+        return string.toUpperCase();
+    }
+
+    private static int multiplyNumbers(int num1, int num2)
+    {
+       return num1 * num2;
+    }
+
+    private static boolean isEven(int num)
+    {
+        return num % 2 == 0;
+    }
+
+    private static int addNumbers(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
